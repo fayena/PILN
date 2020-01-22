54d901890ecef8a52edc18d804227b0f3ab08d7d
fayena <daylene.alford@gmail.com>
2020-01-22 13:00:04 -0600

    Update pilnsetup.sh

--- pilnsetup.sh
+++ pilnsetup.sh
@@ -3,47 +3,58 @@
 #Install Script for PILN
 
 #update pi
-sudo apt get update
-sudo apt get upgrade
+#sudo apt update
+#sudo apt upgrade
 
 #create directories
-su - pi
+
 git clone https://github.com/fayena/PILN.git
-mkdir ./db ./html ./html/images ./html/style
-ln -s /home/pi/PILN/images/hdrback.png /home/pi/html/images/hdrback.png
-ln -s /home/pi/PILN/images/piln.png    /home/pi/html/images/piln.png
-ln -s /home/pi/PILN/style/style.css    /home/pi/html/style/style.css
+sudo mkdir ./db ./html ./html/images ./html/style
+sudo ln -s /home/pi/PILN/images/hdrback.png /home/pi/html/images/hdrback.png
+sudo ln -s /home/pi/PILN/images/piln.png    /home/pi/html/images/piln.png
+sudo ln -s /home/pi/PILN/style/style.css    /home/pi/html/style/style.css
+sudo ln -s /home/pi/PILN/app /home/pi/html/
+sudo chown -R www-data:www-data /home/pi/html
+
+echo "directories created"
 
 #install needed softare
-apt install sqlite3
-apt install ufw
-apt install lighttpd
+
+sudo apt install sqlite3
+sudo apt install ufw
+sudo apt install lighttpd
+
+echo "software installed"
 
 #setup firewall
-ufw allow ssh
-ufw allow http
+sudo ufw allow ssh
+sudo ufw allow http
+echo "firewall setup"
 
 #setup web server (lighttpd)
-cp ./PILN/lighttpd.conf /etc/lighttpd/
+sudo cp /home/pi/PILN/lighttpd/lighttpd.conf /etc/lighttpd/
 cd /etc/lighttpd/conf-enabled
-ln -s ../conf-available/10-cgi.conf .
+sudo ln -s ../conf-available/10-cgi.conf .
 cd
-chown www-data:www-data PILN/html/pilnstat.json
+sudo chown www-data:www-data PILN/html/pilnstat.json
+echo "webserver setup"
 
 #enable raspberry pi interfaces
-raspi-config #enable interfaces ic2 & spi
+sudo raspi-config #enable interfaces ic2 & spi
 lsmod | grep spi
+echo "interfaces enabled"
 
 #install thermocouple amplifier
 
 cd
-git https://github.com/johnrbnsn/Adafruit_Python_MAX31856
+git clone https://github.com/johnrbnsn/Adafruit_Python_MAX31856
 cd Adafruit_Python_MAX31856
 python setup.py install
+echo "thermocouple amplifier installed"
 
 #install database
 
-cp ./PILN/db/PiLN.sqlite3 ./db/PiLN.sqlite3
-chown -R www-data:www-data /home/pi/db
-sqlite3 /home/db/PILN.sqlite3
-.read /home/pi/PILN/docs/PiLN.sql;
+sudo cp ./PILN/db/PiLN.sqlite3 ./db/PiLN.sqlite3
+sudo chown -R www-data:www-data /home/pi/db
+
+echo "database installed"
