9476075c9a6e6e2d5b8b3119830294c1670e24e7
fayena <daylene.alford@gmail.com>
2021-08-14 10:07:15 -0500

    Update pilnsetup.sh

--- pilnsetup.sh
+++ pilnsetup.sh
@@ -37,6 +37,16 @@ sudo cp /home/pi/PILN/lighttpd/lighttpd.conf /etc/lighttpd/
 cd /etc/lighttpd/conf-enabled
 sudo ln -s ../conf-available/10-cgi.conf .
 cd
+#create directories
+
+git clone https://github.com/fayena/PILN.git
+sudo mkdir ./db ./html ./html/images ./html/style ./PILN/log
+sudo ln -s /home/pi/PILN/images/hdrback.png /home/pi/html/images/hdrback.png
+sudo ln -s /home/pi/PILN/images/piln.png    /home/pi/html/images/piln.png
+sudo ln -s /home/pi/PILN/style/style.css    /home/pi/html/style/style.css
+sudo ln -s /home/pi/PILN/app /home/pi/html/
+echo "directories created"
+
 #make sure permissions are correct
 sudo chown -R -L  www-data:www-data /home/pi/html/
 sudo chown pi:pi /home/pi/html/app/pilnstat.json
